e494c8d978cc72b4f6868930fd7ba564aa3d7c2c
dheemanth1994 <64475506+dheemanth1994@users.noreply.github.com>
2020-04-28 17:52:37 +0530

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Atyati.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atyati.Models
+{
+    public class Employee
+    {
+
+
+        [Key]
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+     
+        public string MiddleName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int Salary { get; set; }
+        [Required]
+        public string Role { get; set; }
+
+
+        public DateTime StartDate { get; set; }
+ 
+        public Department DepartmentId { get; set; }
+
+
+
+
+
+    }
+}
